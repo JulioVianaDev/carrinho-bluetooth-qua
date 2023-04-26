f98d2bb32ba9bfc7b18805dbbfa0c5e6d856f01e
JulioVianaDev <JulioVianaDev@gmail.com>
2023-04-26 16:31:53 -0300

    setando as saidas do arduino e colocando velocidade maxima

--- CARRINHO-BLUETOTH-QUA.ino
+++ CARRINHO-BLUETOTH-QUA.ino
@@ -12,9 +12,19 @@ int ENB = 5;
 int IN3 = 7;
 int IN4 = 6;
 
+char t;
+
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(9600);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  analogWrite(ENA,255);
+  analogWrite(ENB,255);
 }
 
 void loop() {
