076a64e93f23500bf9eac80f96c24a8099ff5b70
JulioVianaDev <JulioVianaDev@gmail.com>
2023-04-26 16:51:08 -0300

    QUATRO MOVIMENTOS

--- CARRINHO-BLUETOTH-QUA.ino
+++ CARRINHO-BLUETOTH-QUA.ino
@@ -37,6 +37,26 @@ void loop() {
       digitalWrite(IN2,LOW);
       digitalWrite(IN3,LOW);
       digitalWrite(IN4,HIGH);
+    }else if(t == 'B'){
+      digitalWrite(IN1,LOW);
+      digitalWrite(IN2,HIGH);
+      digitalWrite(IN3,HIGH);
+      digitalWrite(IN4,LOW);
+    }else if(t == 'L'){
+      digitalWrite(IN1,LOW);
+      digitalWrite(IN2,HIGH);
+      digitalWrite(IN3,LOW);
+      digitalWrite(IN4,HIGH);
+    }else if(t == 'R'){
+      digitalWrite(IN1,HIGH);
+      digitalWrite(IN2,LOW);
+      digitalWrite(IN3,HIGH);
+      digitalWrite(IN4,LOW);
+    }else if (t == 'S'){
+      digitalWrite (IN1, LOW);
+      digitalWrite (IN2, LOW);
+      digitalWrite (IN3, LOW);
+      digitalWrite (IN4, LOW);
     }
   }
 }
