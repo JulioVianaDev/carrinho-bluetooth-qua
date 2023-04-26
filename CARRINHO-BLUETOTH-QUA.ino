d246307751c65a2baf0b72bcbb4bcf95321a0286
JulioVianaDev <JulioVianaDev@gmail.com>
2023-04-26 16:28:12 -0300

    criando os motores da ponte h

--- CARRINHO-BLUETOTH-QUA.ino
+++ CARRINHO-BLUETOTH-QUA.ino
@@ -2,6 +2,16 @@
 
 SoftwareSerial bt(0,1);
 
+//LADO ESQUERDO
+int ENA = 10;
+int IN1 = 9;
+int IN2 = 8;
+
+//LADO DIREITO
+int ENB = 5;
+int IN3 = 7;
+int IN4 = 6;
+
 void setup() {
   // put your setup code here, to run once:
   
